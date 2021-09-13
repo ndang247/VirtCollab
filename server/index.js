1eda1910a42283f961146cd7585e530bd64e0c9e
ndang <nathandang73@gmail.com>
2021-09-14 01:56:54 +1000

    Paste sample code

--- server/index.js
+++ server/index.js
@@ -40,6 +40,27 @@ app.post('/', (req, res) => {
     return res.status(200).send('Not a new message request');
 });
 
+// app.post('/', (req, res) => {
+//     const { message, user: sender, type, members } = req.body;
+
+//     if (type === 'message.new') {
+//         members.filter((member) => member.user_id !== sender.id)
+//             .forEach(({ user }) => {
+//                 if (!user.online) {
+//                     twilioClient.messages.create({
+//                         body: `You have a new message from ${message.user.fullName} - ${message.text}`,
+//                         messagingServiceSid: twilioMessagingServiceSid,
+//                         to: user.phoneNumber
+//                     })
+//                         .then(() => console.log('Message sent!'))
+//                         .catch((err) => console.log(err));
+//                 }
+//             })
+//         return res.status(200).send('Message sent!');
+//     }
+//     return res.status(200).send('Not a new message request');
+// });
+
 app.use('/api/auth', authRouter);
 
 const PORT = process.env.PORT || 8080;
